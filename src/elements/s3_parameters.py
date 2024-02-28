bf8c984dffaa3340bfba85373a24def424d15533
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-02-28 18:08:58 +0000

    Add src/elements/s3_parameters.py

---
+++ src/elements/s3_parameters.py
@@ -0,0 +1,38 @@
+"""
+This is data type S3Parameters
+"""
+import typing
+
+
+class S3Parameters(typing.NamedTuple):
+    """
+    The data type class ⇾ S3Parameters
+
+    Attributes
+    ----------
+
+    region_name
+      * The Amazon Web Services region code.
+
+    location_constraint
+      * The region code of the region that the data is limited to.
+
+    access_control_list
+      * Access control list selection.
+
+    internal
+      * The Amazon S3 (Simple Storage Service) bucket that hosts this project's data.
+
+    path_internal_points
+      * The bucket path of the telemetric data.
+
+    path_internal_references
+      * The bucket path of the telemetric data references.
+    """
+
+    region_name: str
+    location_constraint: str
+    access_control_list: str
+    internal: str
+    path_internal_points: str
+    path_internal_references: str
