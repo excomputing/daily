f8861acc5e1de2229231e983dd2bb0824a6ac88c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-02-28 19:27:21 +0000

    Add config.py

---
+++ config.py
@@ -0,0 +1,30 @@
+"""
+Module config
+"""
+import os
+import datetime
+
+
+class Config:
+    """
+    Class Config
+
+    For project settings
+    """
+
+    def __init__(self):
+        """
+        Constructor
+        """
+
+        self.warehouse: str = os.path.join(os.getcwd(), 'warehouse')
+
+        # Realtime
+        self.starting = ''
+        self.ending = datetime.datetime.today().strftime(format='%Y-%m-%d')
+
+        # Devices in focus, via their sequence identifiers
+        # pollutant: Nitrogen Dioxide
+        # area: Edinburgh
+        # sequence (station): 155 (901), 531 (1014), 177 (460), 150 (791) 165(327), 142 (196)
+        self.sequence_id_filter = [155, 531, 177, 150]
