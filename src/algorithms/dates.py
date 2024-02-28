42e5bc409c3c3b5078ffc806ddba4b5778e05c03
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-02-28 18:02:26 +0000

    Update src/algorithms/dates.py

--- src/algorithms/dates.py
+++ src/algorithms/dates.py
@@ -1,15 +1,28 @@
+"""
+Module dates.py
+"""
 import datetime
 import logging
 
 import pandas as pd
 
+import config
+
 
 class Dates:
+    """
+    Notes
+    -----
+
+    Determines the calculation dates.
+    """
 
     def __init__(self):
         """
-
+        The constructor.
         """
+
+        self.__configurations = config.Config()
 
         # logging
         logging.basicConfig(level=logging.INFO,
@@ -17,12 +30,12 @@ class Dates:
                             datefmt='%Y-%m-%d %H:%M:%S')
         self.__logger = logging.getLogger(__name__)
 
-    def exc(self, starting: str = '') -> list[str]:
+    def exc(self) -> list[str]:
 
-        ends = datetime.datetime.today()
+        ends = datetime.datetime.strptime(__date_string=self.__configurations.ending, __format='%Y-%m-%d')
 
-        if starting:
-            starts = datetime.datetime.strptime(starting, '%Y-%m-%d')
+        if self.__configurations.starting:
+            starts = datetime.datetime.strptime(__date_string=self.__configurations.starting, __format='%Y-%m-%d')
             values = pd.date_range(start=starts, end=ends, freq='MS').to_list()
             datestr_ = [str(value.date()) for value in values]
         else:
