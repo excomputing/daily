3c462e30596ea5efd6419c1da7c2dd11d88b16fe
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2024-03-02 22:36:14 +0000

    Update src/functions/serial.py

--- src/functions/serial.py
+++ src/functions/serial.py
@@ -10,14 +10,13 @@ class Serial:
 
     Description
     -----------
-    Presently, this class reads-in local YAML data files; YAML is a data serialisation language.
+    Present, this class reads-in local YAML data files; YAML is a data serialisation language.
     """
 
     def __init__(self):
         """
         Constructor
         """
-        pass
 
     @staticmethod
     def get_dictionary(uri: str) -> dict:
@@ -27,8 +26,8 @@ class Serial:
         :return:
         """
 
-        with open(file=uri, mode='r') as stream:
+        with open(file=uri, mode='r', encoding='utf-8') as stream:
             try:
                 return yaml.load(stream=stream, Loader=yaml.CLoader)
             except yaml.YAMLError as err:
-                raise Exception(err) from err
+                raise err from err
