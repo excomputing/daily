c188bc867cd3191444d1e0563426def67b2e8c2c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2025-01-14 22:50:24 +0000

    Update src/algorithms/reference.py

--- src/algorithms/reference.py
+++ src/algorithms/reference.py
@@ -44,7 +44,7 @@ class Reference:
         uri = f's3://{self.__s3_parameters.internal}/{self.__s3_parameters.path_internal_references}{filename}'
         text = txa.TextAttributes(uri=uri, header=0)
 
-        return self.__streams.api(text=text)
+        return self.__streams.read(text=text)
 
     def __excerpt(self, blob: pd.DataFrame) -> pd.DataFrame:
         """
